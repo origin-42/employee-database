860f92ab737aaed1864e556544be3860c1c36352
Origin 42 <daveplum1991@gmail.com>
2022-05-12 00:36:38 +0800

    updating to make use of async and await

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const {
     Choices
 } = require('./classes/prompts');
 const db = require("./db/connection");
-const query = require('./db/queries_helper')
+const query = require('./db/queries_helper');
 
 
 // Validate Entries
@@ -57,7 +57,7 @@ const addRoleOption = {
     ]
 };
 
-const addAmployeeOption = {
+const addEmployeeOption = {
 
     employeeInfo: [
         new PromptObj("name", "What is the employees first name? ", "input", function (input) {
@@ -69,6 +69,11 @@ const addAmployeeOption = {
     ]
 };
 
+const getDepartments = (deptNames) => [new Choices("departmentSelection", "Which department will this role belong too? \n", "list", deptNames)];
+const getEmployeeName = (employees)=> [new Choices("employName", `Whose role will be updated? \n`, "list", employees)];
+const getRoleName = (name, roleNames) => [new Choices("roleName", `Which role is ${name} being assigned? \n`, "list", roleNames)];
+const checkManagerName = (names) => [new Choices("manager", "Who is this persons Manager? ", "list", names)]
+
 // Construct Inquiry
 // Start Application
 const begin = () => {
@@ -79,184 +84,204 @@ const begin = () => {
         })
 };
 // Provide menu items
-const provideMenu = () => {
-    inquirer.prompt(menuOptions).then((response) => {
-        // Get Department Data
-        if (response.optionsSelect === 'View all departments') {
-            getDeptInfo();
-        } else if (response.optionsSelect === 'Add a department') {
-            promptDept();
-        } else if (response.optionsSelect === 'View all roles') {
-            getRolesInfo();
-        } else if (response.optionsSelect === 'Add a role') {
-            promptRole();
-        } else if (response.optionsSelect === 'View all employees') {
-            getEmploInfo();
-        } else if (response.optionsSelect === 'Add an employee') {
-            promptEmployee();
-        } else if (response.optionsSelect === 'Update an employee role') {
-            updateEmployee();
-        } else if (response.optionsSelect === 'Finish') {
-            // End Program
-            console.log(greenText, "Updated completed. Application terminating.")
-            process.exit();
-        }
-    })
+const provideMenu = async () => {
+    const menuPrompt = await inquirer.prompt(menuOptions);
+    // Get Department Data
+    if (menuPrompt.optionsSelect === 'View all departments') {
+        getDeptInfo();
+    } else if (menuPrompt.optionsSelect === 'Add a department') {
+        promptDept();
+    } else if (menuPrompt.optionsSelect === 'View all roles') {
+        getRolesInfo();
+    } else if (menuPrompt.optionsSelect === 'Add a role') {
+        promptRole();
+    } else if (menuPrompt.optionsSelect === 'View all employees') {
+        getEmploInfo();
+    } else if (menuPrompt.optionsSelect === 'Add an employee') {
+        promptEmployee();
+    } else if (menuPrompt.optionsSelect === 'Update an employee role') {
+        updateEmployee();
+    } else if (menuPrompt.optionsSelect === 'Finish') {
+        // End Program
+        console.log(greenText, "Updated completed. Application terminating.")
+        process.exit();
+    }
 };
 
+// Render Table Info
+const present = async (info) => {
+    spacer();
+    console.table(info);
+    spacer();
+}
+
 // Get Info on Departments
-const getDeptInfo = () => {
-    db.query(query.selectDepartment, (err, response) => {
-        if (err) console.log(err);
-        //Render Department Info Table
-        spacer();
-        console.table(response);
-        return provideMenu();
-    })
+const getDeptInfo = async () => {
+    const departmentData = await db.promise().query(query.selectDepartment)
+    await present(departmentData[0]);
+    return provideMenu();
 };
 
 // Add a Department
-const promptDept = () => {
-    return inquirer.prompt(AddDepartmentOption).then(response => {
-        db.query(query.addDeptName, response.departmentName, (err) => {
-            if (err) console.log(err);
-            return getDeptInfo();
-        })
-    })
+const promptDept = async () => {
+    const addDeptOpt = await inquirer.prompt(AddDepartmentOption);
+    await db.promise().query(query.addDeptName, addDeptOpt.departmentName);
+    return getDeptInfo();
 };
 
 // View Roles
-const getRolesInfo = () => {
-    db.query(query.selectRoles, (err, response) => {
-        if (err) console.log(err);
-        //Render Department Info Table
-        spacer();
-        console.table(response);
-        return provideMenu();
-    })
+const getRolesInfo = async () => {
+    const rolesData = await db.promise().query(query.selectRoles)
+    await present(rolesData[0]);
+    return provideMenu();
 }
 
 // Add a Role
-const promptRole = () => {
-    return inquirer.prompt(addRoleOption.roleInfo)
-        .then(roleInfo => {
-            const {
-                roleName,
-                salary
-            } = roleInfo;
-            db.query(query.selectDepartment, (err, departments) => {
-                if (err) console.log(err);
-                const deptNames = departments.map(dept => dept.name);
-                inquirer.prompt({"name": "departmentSelection", "message": "Which department will this role belong too? ", "type": "list", "choices": deptNames})
-                    .then(departmentAnswer => {
-                        const { departmentSelection } = departmentAnswer;
-                        db.query(query.findDeptId, departmentSelection, (err, response) => {
-                            db.query(query.addRoleName, [roleName, salary, response[0].id], (err) => {
-                                if (err) console.log(err);
-                                return getRolesInfo();
-                            })
-                        })
-                    });
-            });
-        });
+const promptRole = async () => {
+    
+    // Return user input of Role & Salary.
+    const rolesInfo = await inquirer.prompt(addRoleOption.roleInfo);
+    const { roleName, salary } = rolesInfo;
+    const getAllDepts = await db.promise().query(query.selectDepartment);
+    const deptNames = getAllDepts[0].map(dept => dept.name);
+    
+    // return user input of Department the role will belong to.
+    const departmentAnswer = await inquirer.prompt(getDepartments(deptNames));
+    const { departmentSelection } = departmentAnswer;
+    
+    // Add data to Department table
+    const findDeptId = await db.promise().query(query.findDeptId, departmentSelection);
+    await db.promise().query(query.addRoleName, [roleName, salary, findDeptId[0][0].id]);
+
+    getRolesInfo();
 };
 
 // View Roles
-const getEmploInfo = () => {
-    db.query(query.selectEmployees, (err, response) => {
-        if (err) console.log(err);
-        //Render Department Info Table
-        spacer();
-        console.table(response);
-        return provideMenu();
-    })
+const getEmploInfo = async () => {
+    const employData = await db.promise().query(query.selectEmployees)
+    await present(employData[0]);
+    return provideMenu();
 }
 
 // Add an employee
-const promptEmployee = () => {
-    return inquirer.prompt(addAmployeeOption.employeeInfo)
-        .then(info => {
-            const {
-                name,
-                sirname
-            } = info;
-            db.query(query.getRoles, (err, roles) => {
-                const roleNames = roles.map(role => role.title);
-                inquirer.prompt({"name": "roleName", "message": `Which role is ${name} being assigned? `, "type": "list", "choices": roleNames})
-                    .then(role => {
-                        db.query(query.checkForManagers, (err, manager_name) => {
-                            const names = manager_name.map(x => x.managers_name).filter(y => y)
-                            names.push('New Manager')
-                            inquirer.prompt({"name": "manager", "message": "Who is this persons Manager? ", "type": "list", "choices": names})
-                                .then(results => {
-                                    db.query(query.getManagerId, role.roleName, (err, response) => {
-                                        const { manager } = results;
-                                        const arr = [name, sirname, response[0].id];
-                                        if (manager === "New Manager") {
-                                            db.query(query.addNewManager, arr, (err) => {
-                                                if (err) console.log(err);
-                                                return getEmploInfo();
-                                            })
-                                        } else {
-                                            arr.push(response[0].id)
-                                            db.query(query.addNewEmployee, arr, (err) => {
-                                                if (err) console.log(err);
-                                                return getEmploInfo();
-                                            })
-                                        }
-                                    })
-                                })
-                        })
-                    })
-            })
+const promptEmployee = async () => {
+
+    // Get existing data from employee & role
+    const getAllData = await db.promise().query(query.getemplAndRoleData); // Get all data
+
+    // Prompts
+    // Get first name and last name adding
+    const addEmplOpt = await inquirer.prompt(addEmployeeOption.employeeInfo); 
+    const { name, sirname } = addEmplOpt; // Save employee name
+
+    // Get new role
+    const getRoles = await db.promise().query(query.getRoles);
+    const roleNames = getRoles[0].map(role => role.title);
+    const assignRole = await inquirer.prompt(getRoleName(name, roleNames));
+
+    // Check if this role belongs to a manager
+    const roles = getAllData[0].filter(person => person.title === assignRole.roleName ? person : false);
+
+    console.log(roles[0])
+    // Send data to DB
+    if (roles[0].managers_name) {
+        await db.promise().query(query.addNewEmployee, [name, sirname, roles[0].role_id, roles[0].manager_id]) // Add an employee
+    } else {
+        await db.promise().query(query.addNewManager, [name, sirname, roles[0].role_id]) // Add a manager
+    }
+
+    return getEmploInfo();
+}
+
+const updateEmployee = async () => {
+
+    const getAllData = await db.promise().query(query.getAllData); // Get all data
+    const getAllEmployees = await db.promise().query(query.selectEmployees) // get all employees
+    const getAllRoles = await db.promise().query(query.getRoles) // get distinct managers
+
+    const employeeNames = getAllEmployees[0].map(employee => employee.employee_name) // Get employee names
+    const roleNames = getAllRoles[0].map(role => role.title) // Get role names
+
+    // Prompts
+    const selectedEmployee = await inquirer.prompt(getEmployeeName(employeeNames)) // Prompt for employee
+    const selectedRole = await inquirer.prompt(getRoleName(selectedEmployee.employName, roleNames)) // Prompt for role
+    
+    const employeeInfo = getAllData[0].find(employee => employee.employee_name == selectedEmployee.employName) // Get current info
+    const employeeNewRole = getAllData[0].find(role => role.title == selectedRole.roleName) // Get new info
+
+    // Manager selection clauses 
+    if (employeeInfo.role_id === employeeNewRole.role_id) {
+        // If the same role is selected, return menu.
+        console.log(redText, `You've selected the same role. No updates made.`);
+         return getEmploInfo();
+    } else if (!employeeInfo.manager_id && !employeeNewRole.manager_id) {
+        // Old role needs filling & new roles current manager needs their role reassigned
+        await updateOldManager(employeeInfo, getAllData, employeeNames, roleNames);
+
+        console.log(redText, `The ${employeeInfo.title} role remains unnassigned. A new manager is required. Please update the position.\n`);
+        await updateNewManager(employeeNewRole, getAllData, employeeNames);
+    } else if (!employeeInfo.manager_id) {
+        // Old role needs filling
+        await updateOldManager(employeeInfo, getAllData, employeeNames, roleNames);
+    } else if (!employeeNewRole.manager_id) {
+        // New roles current manager needs their role reassigned
+        console.log(redText, `The ${employeeInfo.title} role belonged to ${employeeNewRole.managers_name}. A new manager is required. Please update the position.\n`);
+        await updateNewManager(employeeNewRole, getAllData, employeeNames);
+    }
+
+    
+    // Update employee
+    console.log(greenText, `${employeeInfo.employee_name} has been assigned as ${employeeNewRole.title}.`);
+    await db.promise().query(query.updateEmployeeRole, [employeeNewRole.role_id, employeeNewRole.manager_id, employeeInfo.employee_id]);
+    return getEmploInfo();
+}
+
+const updateOldManager = async (oldEmployee, data, employeeNames) => {
+    // Remove selected employee and managers from list
+    const newNames = await formatEmployees(oldEmployee, employeeNames)
+    
+    // Prompt for new employee to add to old, unnassigned role
+    console.log(redText, `\nSomebody will need to replace ${oldEmployee.employee_name} as the ${oldEmployee.title}.\n`)
+    const selectedEmployee = await inquirer.prompt(getEmployeeName(newNames)) // Prompt for employee
+    
+    const employeeInfo = data[0].find(employee => employee.employee_name == selectedEmployee.employName) // Get current info
+    
+    // Assign the old role
+    console.log(greenText, `${employeeInfo.employee_name} has been assigned as ${oldEmployee.title}.\n`);
+    await db.promise().query(query.updateEmployeeRole, [employeeInfo.role_id, employeeInfo.manager_id, oldEmployee.employee_id]);
+  
+}
+const updateNewManager = async (employeeInfo, data, employeeNames) => {
+    // Remove selected employee and managers from list
+    const newNames = await formatEmployees(oldEmployee, employeeNames)
+
+    // Prompt for the role which the current manager will be taking
+    const selectedEmployee = await inquirer.prompt(getEmployeeName(newNames)) // Prompt for employee
+    
+    employeeInfo = data[0].find(employee => employee.employee_name == selectedEmployee.employName) // Get current info
+
+    // Assign the old role
+    console.log(greenText, `${employeeInfo.employee_name} has been assigned as ${employeeInfo.title}.\n`);
+    await db.promise().query(query.updateEmployeeRole, [employeeInfo.role_id, employeeInfo.manager_id, employeeInfo.employee_id]);
+
+}
+
+const formatEmployees = async (oldEmployee, employeeNames) => {
+    const managers = await db.promise().query(query.checkForManagers);
+   
+    // Remove selected employee from list
+    let newNames = employeeNames.filter(employee => employee != oldEmployee.employee_name) // refresh employee list
+    newNames.forEach(name => {
+        console.log(name)
+        managers[0].forEach(manager => {
+            if (name === manager.managers_name) {
+                let index = newNames.indexOf(name);
+                newNames.splice(index, 1);
+            }
         })
-}
-
-const updateEmployee = () => {
-    db.query(query.selectEmployees, (err, employees_response) => {
-        if (err) console.log(err);
-        const employees = employees_response.map(x => x.employee_name)
-        inquirer.prompt({"name": "selectdEmployee", "message": "Which employee? ", "type": "list", "choices": employees})
-            .then(employee => {
-                const employeeName = employee.selectdEmployee;
-                db.query(query.selectRoles, (err, roles_response) => {
-                    if (err) console.log(err);
-                    const roles = roles_response.map(x => x.title);
-                    inquirer.prompt({"name": "selectRole", "message": `Which role will ${employeeName} be assigned? `, "type": "list", "choices": roles})
-                        .then(role => {
-                            // Logic for assigning employee based off passed in information.
-                            const employeeArr = employees_response.filter(x => {
-                                if (x.employee_name === employeeName) {
-                                    return x;
-                                }  
-                            })
-                            roles_response.forEach(x => {
-                                if (x.title === employeeArr[0].title) {
-                                    employeeArr[0].role_id = x.id;
-                                }
-                                if (role.selectRole === x.title) {
-                                    employeeArr[0].newRole = x.id;
-                                }
-                            })
-                            db.query(query.confirmManager, employeeArr[0].newRole, (err, manager_info) => {
-                                if (manager_info[0].manager_id) {
-                                    // update as employee
-                                    const updateEmployeeQuery = [employeeArr[0].newRole, employeeArr[0].employee_id];
-                                } else {
-                                    // Update as manager
-                                    const updateEmployeeQuery = [employeeArr[0].newRole, employeeArr[0].employee_id];
-                                }
-                            })
-                            
-                        })
-                })
-            })
     })
-                    // Update Employee
-
-                    // Things to check: Check of other employees and see how they update also
-
-                    
+    return newNames;
 }
+
 
 module.exports = begin;
--- db/queries_helper.js
+++ db/queries_helper.js
@@ -38,11 +38,34 @@ VALUES
 
 // Employee queries
 
+const getemplAndRoleData = `
+SELECT employee.id AS employee_id, concat(employee.first_name, " ", employee.last_name) AS employee_name, manager.id AS manager_id, concat(manager.first_name, " ", manager.last_name) AS managers_name, title, employee.role_id
+FROM Employee employee
+LEFT JOIN Employee manager
+ON employee.manager_id = manager.id
+LEFT JOIN role
+ON employee.role_id = role.id`;
+
+const getAllData = `
+SELECT employee.id AS employee_id, concat(employee.first_name, " ", employee.last_name) AS employee_name, manager.id AS manager_id, concat(manager.first_name, " ", manager.last_name) AS managers_name, title, employee.role_id, role.department_id, department.name
+FROM Employee employee
+LEFT JOIN Employee manager
+ON employee.manager_id = manager.id
+LEFT JOIN role
+ON employee.role_id = role.id
+LEFT JOIN department
+ON department.id = role.department_id`;
+
 const checkForManagers = `
 SELECT DISTINCT concat(manager.first_name, " ", manager.last_name) AS managers_name 
 FROM Employee employee
 LEFT JOIN Employee manager
 ON employee.manager_id = manager.id`;
+
+const checkManPositions = `
+SELECT title
+FROM role
+WHERE id = `;
 
 const getManagerId = `
 SELECT id
@@ -58,15 +81,20 @@ const addNewEmployee = `
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
 (?, ?, ?, ?)`;
+
 const addNewManager = `
 INSERT INTO employee (first_name, last_name, role_id)
 VALUES
 (?, ?, ?)`;
 
-// const updateEmployeeRole = `
-// UPDATE employee
-// SET role_id = ?, manager_id = ?
-// WHERE id = ?`;
+const updateEmployeeRole = `
+UPDATE employee
+SET role_id = ?, manager_id = ?
+WHERE id = ?`;
+
+const removeRole = `
+DELETE FROM role 
+WHERE id = ?`;
 
 module.exports = {
     selectDepartment,
@@ -77,9 +105,12 @@ module.exports = {
     addRoleName,
     getRoles,
     checkForManagers,
+    checkManPositions,
+    getemplAndRoleData,
+    getAllData,
     addNewEmployee,
     confirmManager,
     addNewManager,
     getManagerId,
-    updateEmployeeRole
+    updateEmployeeRole,
 }
